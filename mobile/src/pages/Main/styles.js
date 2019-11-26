25325593170e67fccd8277318d76c2ea72003f57
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-11-26 17:34:51 -0300

    fix styles main

--- mobile/src/pages/Main/styles.js
+++ mobile/src/pages/Main/styles.js
@@ -4,6 +4,8 @@ import {RectButton} from 'react-native-gesture-handler';
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ButtonTeacher = styled(RectButton)`
@@ -15,7 +17,11 @@ export const ButtonTeacher = styled(RectButton)`
   padding: 0 12px;
   opacity: ${props => (props.loading ? 0.7 : 1)};
   height: 46px;
+  width: 90%;
   margin-bottom: 10px;
 `;
 
-export const ButtonTeacherText = styled.Text``;
+export const ButtonTeacherText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+`;
