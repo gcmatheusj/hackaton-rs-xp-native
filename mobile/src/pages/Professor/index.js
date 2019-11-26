5ae1850ad2ffefa34eb2b7884678a308316f5127
gcmatheusj <goca1377@gmail.com>
2019-11-26 17:52:40 -0300

    professores

--- mobile/src/pages/Professor/index.js
+++ mobile/src/pages/Professor/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import {Container, Lista, Atividade, Nome, Data} from './styles';
 
-export default function Professor() {
+const atividades = [
+  {
+    id: 1,
+    nome: 'Atividade 1',
+    data: '2019-11-26',
+  },
+  {
+    id: 2,
+    nome: 'Atividade 2',
+    data: '2019-11-27',
+  },
+  {
+    id: 3,
+    nome: 'Atividade 3',
+    data: '2019-11-28',
+  },
+];
+
+export default function Professor({navigation}) {
   return (
-    <Text>Professor</Text>
+    <Container>
+      <Lista
+        data={atividades}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => (
+          <Atividade onPress={() => navigation.navigate('Detalhes')}>
+            <Nome>{item.nome}</Nome>
+            <Data>{item.data}</Data>
+          </Atividade>
+        )}
+      />
+    </Container>
   );
 }
---
+++ mobile/src/pages/Professor/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Lista = styled.FlatList`
+  padding: 20px;
+`;
+
+export const Atividade = styled.View`
+  align-self: stretch;
+  height: 65px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  background: #999;
+`;
+
+export const Nome = styled.Text``;
+
+export const Data = styled.Text``;
