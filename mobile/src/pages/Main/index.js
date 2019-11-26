c3a1cc2bb23a4889880a6e165db103507dcecde5
willmorim <pdbawilliam@gmail.com>
2019-11-26 16:18:22 -0300

    add react-navigation, reactotron, criação de telas iniciais

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Main extends Component {
+  render() {
+    return <Text>tela 1</Text>;
+  }
+}
